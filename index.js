b1f5ca6da18a43154c328c7063ab4b386ab65b8b
Ayush Nene <ayush3108@gmail.com>
2018-12-14 11:57:40 -0500

    setup static assets

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import hbs from "express-handlebars"
+
+const app = express();
+
+
+
+// view engine setup
+app.engine("hbs", hbs({
+    extname: ".hbs",
+    defaultLayout: "main"
+}))
+
+app.set("view engine", "hbs")
+
+// public directories
+app.use(express.static(__dirname + '/public'))
+
+// default router
+app.get("/", (req, res) => {
+    res.render('home', {title: "Homepage"})
+})
+
+app.listen(3000, () => console.log("Listening on port 3000"))
