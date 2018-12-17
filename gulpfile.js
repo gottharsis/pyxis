b7bdb862e89dc7621708500b868a2b444f1e6f0a
Ayush Nene <ayush3108@gmail.com>
2018-12-17 12:04:08 -0500

    stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,90 @@
-'use strict'
+"use strict"
 var gulp = require("gulp"),
-    autoprefixer = require("gulp-autoprefixer"),
-    concat = require("gulp-concat"),
-    sass = require("gulp-sass"),
-    sourcemaps = require("gulp-sourcemaps"),
-    babel = require("gulp-babel"),
-    nodemon = require("gulp-nodemon"),
-    bs = require("browser-sync")
+	autoprefixer = require("gulp-autoprefixer"),
+	concat = require("gulp-concat"),
+	sass = require("gulp-sass"),
+	sourcemaps = require("gulp-sourcemaps"),
+	babel = require("gulp-babel"),
+	nodemon = require("gulp-nodemon"),
+	bs = require("browser-sync")
 
 gulp.task("sass", () => {
-        return gulp.src("./scss/*.scss")
-            .pipe(sourcemaps.init())
-            .pipe(sass().on("error", sass.logError))
-            .pipe(autoprefixer())
-            .pipe(concat("styles.css"))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest("public/css/"))
-    }
-)
+	return gulp
+		.src("./scss/*.scss")
+		.pipe(sourcemaps.init())
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer())
+		.pipe(concat("styles.css"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("public/css/"))
+})
 
 gulp.task("sass:bs", function() {
-    return gulp.src("./scss/*.scss")
-            .pipe(sourcemaps.init())
-            .pipe(sass().on("error", sass.logError))
-            .pipe(autoprefixer())
-            .pipe(concat("styles.css"))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest("public/css/"))
-            .pipe(bs.stream())
+	return gulp
+		.src("./scss/*.scss")
+		.pipe(sourcemaps.init())
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer())
+		.pipe(concat("styles.css"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("public/css/"))
+		.pipe(bs.stream())
 })
 
 gulp.task("js", () => {
-    return gulp.src("./javascripts/*.js")
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat("script.js"))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./public/js/"))
+	return gulp
+		.src("./javascripts/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.pipe(concat("script.js"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("./public/js/"))
 })
 
 gulp.task("js:bs", () => {
-    return gulp.src("./javascripts/*.js")
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat("script.js"))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./public/js/"))
-        .pipe(bs.stream())
+	return gulp
+		.src("./javascripts/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.pipe(concat("script.js"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("./public/js/"))
+		.pipe(bs.stream())
 })
 
 gulp.task("watch", () => {
-    gulp.watch("scss/*.scss", gulp.series('sass'))
-    gulp.watch("javascripts/*.js", gulp.series('js'))
-});
-
-
-gulp.task("nodemon", function(cb) {
-    var started = false;
-
-    return nodemon({
-        script: "index.js",
-        exec: "babel-node",
-        watch: ["index.js", "routes/**/*"]
-    }).on("start", function() {
-        if (!started) {
-            cb();
-            started = true;
-        }
-    })
+	gulp.watch("scss/*.scss", gulp.series("sass"))
+	gulp.watch("javascripts/*.js", gulp.series("js"))
 })
 
-gulp.task("browserSync", gulp.series("nodemon", function() {
-    bs.init({
-        proxy: "http://localhost:3000",
-        port: 7000,
-        online: false
-    })
+gulp.task("nodemon", function(cb) {
+	var started = false
 
-    gulp.watch("scss/*.scss", gulp.series("sass:bs"))
-    gulp.watch("javascripts/*.js", gulp.series("js:bs"))
-    gulp.watch("views/**/*").on("change", bs.reload)
-}))
+	return nodemon({
+		script: "index.js",
+		exec: "babel-node",
+		watch: ["index.js", "routes/**/*", "views/**/*"]
+	}).on("start", function() {
+		if (!started) {
+			cb()
+			started = true
+		}
+	})
+})
+
+gulp.task(
+	"browserSync",
+	gulp.series("nodemon", function() {
+		bs.init({
+			proxy: "http://localhost:3000",
+			port: 7000,
+			online: false
+		})
+
+		gulp.watch("scss/*.scss", gulp.series("sass:bs"))
+		gulp.watch("javascripts/*.js", gulp.series("js:bs"))
+		gulp.watch("views/**/*").on("change", bs.reload)
+	})
+)
 
 gulp.task("default", gulp.series("browserSync"))
