d2298eea3875373bb8c50859fb2b7f873e66c394
Ayush Nene <ayush3108@gmail.com>
2019-01-15 23:44:13 -0500

    switch product from interface to type

--- models/product.js
+++ models/product.js
@@ -1,25 +1,9 @@
 // @flow
 
-export interface IProduct {
-	id: number;
-	name: string;
-	price: number;
-	imageURL: string;
-	description: string;
-}
-
-class Product {
-	name: string
-	id: number
-	price: number
-	imageURL: string
+export type Product = {
+	id: number,
+	name: string,
+	price: number,
+	imageURL: string,
 	description: string
-
-	constructor({ id, name, price, imageURL, description }: IProduct) {
-		this.id = id
-		this.name = name
-		this.price = price
-		this.imageURL = imageURL
-		this.description = description
-	}
 }
