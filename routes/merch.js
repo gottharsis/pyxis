eafa80cf548cb42dff30e4a91afe313cd29b44de
Ayush Nene <ayush3108@gmail.com>
2019-01-15 23:44:01 -0500

    add types to merch router

--- routes/merch.js
+++ routes/merch.js
@@ -1,19 +1,24 @@
 // @flow
 import { getProducts, getProduct } from "../db/product"
+import type { $Request, $Response } from "express"
 import { Router } from "express"
 
 const router = new Router()
 
-router.get("/merch", async (req, res) => {
-	const items = await getProducts()
-	res.render("merch", { title: "Merch", items })
+router.get("/merch", async (req: $Request, res: $Response) => {
+	try {
+		const items = await getProducts()
+		res.render("merch", { title: "Merch", items })
+	} catch (e) {
+		res.status(400).send("Merch error")
+	}
 })
 
-router.get("/view-item", (req, res) => {
+router.get("/view-item", (req: $Request, res: $Response) => {
 	res.redirect("/merch")
 })
 
-router.get("/view-item/:id", async (req, res) => {
+router.get("/view-item/:id", async (req: $Request, res: $Response) => {
 	const id = req.params.id
 	const item = await getProduct(id)
 	const { name } = item
