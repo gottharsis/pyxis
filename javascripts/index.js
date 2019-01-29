72d0d4350c86347643a28a38ce48f206217f9323
Ayush Nene <ayush3108@gmail.com>
2019-01-29 15:34:54 -0500

    Enable all javascripts

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,14 +1,14 @@
-// import heroVue from "./hero.js"
-// import headerVue from "./header"
-// import homepagePlans from "./homepage-plans"
+import heroVue from "./hero.js"
+import headerVue from "./header"
+import homepagePlans from "./homepage-plans"
 import viewProduct from "./view-product.js"
 import cartVue from "./cart"
 import notification from "./notification"
 
 window.vues = {
-	// heroVue,
-	// headerVue,
-	// homepagePlans,
+	heroVue,
+	headerVue,
+	homepagePlans,
 	viewProduct,
 	cartVue,
 	notification
