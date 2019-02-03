30f4dcdb6ee9ff88075011e611f77ceff8e024c6
Ayush Nene <ayush3108@gmail.com>
2019-02-03 17:49:46 -0500

    edited db config to work on heroku

--- db/database.js
+++ db/database.js
@@ -2,15 +2,24 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-const knex = require("knex")({
-	client: "pg",
-	connection: {
-		host: process.env.PGHOST,
-		user: process.env.PGUSER,
-		password: process.env.PGPASSWORD,
-		database: process.env.PGDATABASE
-	}
-})
+let knex
+
+if (process.env.NODE_ENV === "production") {
+	knex = require("knex")({
+		client: "pg",
+		connection: process.env.DATABASE_URL + "?ssl=true"
+	})
+} else {
+	knex = require("knex")({
+		client: "pg",
+		connection: {
+			host: process.env.PGHOST,
+			user: process.env.PGUSER,
+			password: process.env.PGPASSWORD,
+			database: process.env.PGDATABASE
+		}
+	})
+}
 
 // for testing only
 export const destroyConnection = () => knex.destroy()
