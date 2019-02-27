efd53e42c2ac73db07ee6284e523596e8e8363c2
Ayush Nene <ayush3108@gmail.com>
2019-02-27 07:49:36 -0500

    Added items and images; changed seed to reflect actual items not random

--- seeds/01_products.js
+++ seeds/01_products.js
@@ -1,18 +1,5 @@
-const faker = require("faker")
-
-const createFakeProduct = () => ({
-	name: faker.commerce.productName(),
-	price: faker.commerce.price(),
-	imageURL: faker.image.imageUrl(250, 250),
-	description: faker.lorem.sentence()
-})
+const items = require("../items.json")
 exports.seed = function(knex, Promise) {
 	// Inserts seed entries
-	const fakeData = []
-	const desiredFakeData = 50
-
-	for (let i = 0; i < desiredFakeData; i++) {
-		fakeData.push(createFakeProduct())
-	}
-	return knex("products").insert(fakeData)
+	return knex("products").insert(items)
 }
